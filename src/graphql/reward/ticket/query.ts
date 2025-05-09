6f2c0efa90ac845e8d4f64c0d791f612bff27fe9
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-05-09 18:44:28 +0900

    feat: create graphql

--- src/graphql/reward/ticket/query.ts
+++ src/graphql/reward/ticket/query.ts
@@ -13,22 +13,6 @@ export const GET_TICKETS = gql`
   }
 `;
 
-export const VIEW_TICKET_CLAIM = gql(`
-  query ticketClaimLink($id: ID!) {
-    ticketClaimLink(id: $id) {
-      qty
-      status
-      issuer {
-        owner {
-          id
-          name
-          image
-        }
-      }
-    }
-  }
-`);
-
 export const GET_TICKET = gql`
   query GetTicket($id: ID!) {
     ticket(id: $id) {
