aed5c60eae580db2243bc9ee752dc065bcfb9aec
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-05-11 14:59:44 +0900

    feat: create reservation presenter

--- src/graphql/experience/reservation/query.ts
+++ src/graphql/experience/reservation/query.ts
@@ -1,8 +1,11 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 import { RESERVATION_FRAGMENT } from "@/graphql/experience/reservation/fragment";
 import { SLOT_FRAGMENT } from "@/graphql/experience/opportunitySlot/fragment";
 import { PARTICIPATION_FRAGMENT } from "@/graphql/experience/participation/fragment";
 import { OPPORTUNITY_FRAGMENT } from "@/graphql/experience/opportunity/fragment";
+import { PLACE_FRAGMENT } from "@/graphql/location/place/fragment";
+import { USER_FRAGMENT } from "@/graphql/account/user/fragment";
+import { ARTICLE_FRAGMENT } from "@/graphql/content/article/fragment";
 
 export const GET_RESERVATIONS = gql`
   query GetReservations {
@@ -25,15 +28,27 @@ export const GET_RESERVATION = gql`
         ...OpportunitySlotFields
         opportunity {
           ...OpportunityFields
+          createdByUser {
+            ...UserFields
+            articlesAboutMe {
+              ...ArticleFields
+            }
+          }
+          place {
+            ...PlaceFields
+          }
         }
       }
-      participations {
-        ...ParticipationFields
-      }
+    }
+    participations {
+      ...ParticipationFields
     }
   }
   ${RESERVATION_FRAGMENT}
   ${SLOT_FRAGMENT}
   ${OPPORTUNITY_FRAGMENT}
+  ${PLACE_FRAGMENT}
+  ${USER_FRAGMENT}
+  ${ARTICLE_FRAGMENT}
   ${PARTICIPATION_FRAGMENT}
-`; 
+`;
