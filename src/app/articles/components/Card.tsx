0850129c06a7762e19fbff933ad790e6d70f35c6
kibidan5 <yokinist+hopin@gmail.com>
2025-05-13 18:29:18 +0900

    fix(ui): tweak ArticleCard style

--- src/app/articles/components/Card.tsx
+++ src/app/articles/components/Card.tsx
@@ -3,10 +3,8 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { format } from "date-fns";
-import { ja } from "date-fns/locale";
 import { TArticleCard, TArticleWithAuthor } from "@/app/articles/data/type";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import CategoryBadge from "@/app/articles/components/CategoryBadge";
 
 interface ArticleCardProps {
@@ -28,28 +26,11 @@ const ArticleCard: React.FC<ArticleCardProps> = ({ article, showCategory }) => {
             <Image src={article.thumbnail} alt={article.title} fill className="object-cover" />
           </div>
         )}
-        <CardContent>
-          <h3 className="text-xl font-bold mb-2">{article.title}</h3>
-          {article.publishedAt && !isNaN(new Date(article.publishedAt).getTime()) && (
-            <div className="text-muted-foreground text-sm mb-2">
-              公開: {format(new Date(article.publishedAt), "yyyy年M月d日", { locale: ja })}
-            </div>
-          )}
-          <p className="text-foreground text-sm line-clamp-3">{article.introduction}</p>
-          { "author" in article && article.author && (
-            <div className="flex items-center mt-4">
-              <div className="relative w-8 h-8 rounded-full overflow-hidden mr-2">
-                <Image
-                  src={article.author.image || "/placeholder-avatar.svg"}
-                  alt={article.author.name}
-                  fill
-                  className="object-cover"
-                />
-              </div>
-              <span className="text-sm font-medium">{article.author.name}</span>
-            </div>
-          )}
-        </CardContent>
+        {/* #NOTE: CardContent の padding では上部がなく、かつ余白も大きかったので、他 component への影響を踏まえて、自前で設定 */}
+        <div className="p-4">
+          <h3 className="text-title-md line-clamp-2 mb-2">{article.title}</h3>
+          <p className="text-body-sm text-caption line-clamp-2">{article.introduction}</p>
+        </div>
       </Card>
     </Link>
   );
