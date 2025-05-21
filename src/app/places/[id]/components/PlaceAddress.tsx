87f748331ac5ab737241811a45df1443813acd7e
kibidan5 <yokinist+hopin@gmail.com>
2025-05-21 17:27:01 +0900

    fix(maps): always use address for Google Maps embed instead of coordinates

--- src/app/places/[id]/components/PlaceAddress.tsx
+++ src/app/places/[id]/components/PlaceAddress.tsx
@@ -2,10 +2,12 @@
 import { IPlaceDetail } from "@/app/places/data/type";
 
 const PlaceAddress = ({ detail }: { detail: IPlaceDetail }) => {
-  const mapUrl =
-    detail.latitude && detail.longitude
-      ? `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${detail.latitude},${detail.longitude}`
-      : `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(detail.address)}`;
+  // #NOTE: strapi に保存された緯度経度は若干位置がずれるため、住所からマップを表示している
+  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(detail.address)}`;
+  // const mapUrl =
+  //   detail.latitude && detail.longitude
+  //     ? `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${detail.latitude},${detail.longitude}`
+  //     : `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(detail.address)}`;
 
   return (
     <div className="px-4 pt-6 pb-8 max-w-mobile-l mx-auto space-y-4">
