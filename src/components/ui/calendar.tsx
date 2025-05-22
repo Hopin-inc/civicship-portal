ffd9a395372c5425bc2ab587137b95eceed45ada
kibidan5 <yokinist+hopin@gmail.com>
2025-05-22 13:06:11 +0900

    fix: calendar ui

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,22 +3,19 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
+import { ja } from "date-fns/locale";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
-}: CalendarProps) {
+function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3 mx-auto", className)}
+      locale={ja}
       classNames={{
         months: "flex flex-col space-y-4 mx-auto",
         month: "space-y-4 mx-auto",
@@ -33,30 +30,30 @@ function Calendar({
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
-        head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+        head_cell: "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "tertiary" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100",
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 border-none",
         ),
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground rounded-full",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
           "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-primary-foreground aria-selected:text-primary rounded-none border-x-0",
-        day_range_start: "aria-selected:bg-primary aria-selected:text-white rounded-l-full rounded-r-none !text-white border-none",
-        day_range_end: "aria-selected:bg-primary aria-selected:text-white rounded-r-full rounded-l-none !text-white border-none",
+        day_range_start:
+          "aria-selected:bg-primary aria-selected:text-white !rounded-tr-none !rounded-br-none !text-white border-none",
+        day_range_end:
+          "aria-selected:bg-primary aria-selected:text-white !rounded-tl-none !rounded-bl-none !text-white border-none",
         day_hidden: "invisible",
         ...classNames,
       }}
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
