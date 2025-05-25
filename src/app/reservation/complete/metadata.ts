c0ce54717ab80bc9a126c75e51d08e64f78ce417
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-05-25 22:23:04 +0900

    Add layouts and metadata for reservation flow pages
    Introduced layout components for `select-date`, `confirm`, and `complete` pages under the reservation flow. Added corresponding metadata files to define titles, descriptions, and Open Graph settings for better user

---
+++ src/app/reservation/complete/metadata.ts
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+import { DEFAULT_OPEN_GRAPH_IMAGE } from "@/lib/metadata/defalut";
+
+export const metadata: Metadata = {
+  title: "申込完了 | NEO四国88祭",
+  description: "お申し込みありがとうございます。参加を楽しみにお待ちください！",
+  openGraph: {
+    type: "website",
+    title: "申込完了 | NEO四国88祭",
+    description: "お申し込みありがとうございます。参加を楽しみにお待ちください！",
+    images: DEFAULT_OPEN_GRAPH_IMAGE,
+  },
+};
