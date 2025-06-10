28549b1742928be61c5a3eb1205566ac1e4cace1
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-06-11 00:28:43 +0900

    Refactor FeaturedSection to use a wrapper for the slider
    Extracted ActivitiesFeaturedSlider into a separate wrapper component to simplify and clean up FeaturedSection. This improves modularity and enables better handling of client-side rendering for the slider.

--- src/app/activities/components/FeaturedSection/FeaturedSection.tsx
+++ src/app/activities/components/FeaturedSection/FeaturedSection.tsx
@@ -1,10 +1,8 @@
 import { ActivityCard } from "@/app/activities/data/type";
 import FeaturedSectionSkeleton from "@/app/activities/components/FeaturedSection/FeaturedSectionSkeleton";
 import HeroHeader from "@/app/activities/components/FeaturedSection/HeroHeader";
-import dynamic from "next/dynamic";
 import OpportunityImageSSR from "@/app/activities/components/FeaturedSection/ImageSSR";
-
-const ActivitiesFeaturedSlider = dynamic(() => import("./FeaturedSlider"), { ssr: false });
+import ActivitiesFeaturedSliderWrapper from "@/app/activities/components/FeaturedSection/FeaturedSlideWrapper";
 
 interface Props {
   opportunities: ActivityCard[];
@@ -25,7 +23,7 @@ export default function ActivitiesFeaturedSection({
     <section className="relative h-[70vh] w-full overflow-hidden mb-12">
       <HeroHeader />
       <OpportunityImageSSR image={firstImage} title={firstTitle} />
-      <ActivitiesFeaturedSlider opportunities={opportunities} />
+      <ActivitiesFeaturedSliderWrapper opportunities={opportunities} />
     </section>
   );
 }
