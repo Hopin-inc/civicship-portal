c0d4a4e20df6e418569e532cb3a5379359800bc8
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-06-11 01:15:05 +0900

    test only SSR

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,18 +1,18 @@
 import { fetchFeaturedAndCarousel } from "./data/fetchActivities";
-import ActivitiesPageClient from "@/app/activities/components/ActivitiesPageClient";
+import ActivitiesFeaturedSection from "@/app/activities/components/FeaturedSection/FeaturedSection";
 
 export default async function ActivitiesPage() {
   const { featuredCards, upcomingCards, loading } = await fetchFeaturedAndCarousel();
   return (
     <>
       <div className="min-h-screen">
-        {/*<ActivitiesFeaturedSection opportunities={featuredCards} isInitialLoading={loading} />*/}
+        <ActivitiesFeaturedSection opportunities={featuredCards} isInitialLoading={loading} />
         {/*<ActivitiesCarouselSection*/}
         {/*  title="もうすぐ開催予定"*/}
         {/*  opportunities={upcomingCards}*/}
         {/*  isInitialLoading={loading}*/}
         {/*/>*/}
-        <ActivitiesPageClient />
+        {/*<ActivitiesPageClient />*/}
       </div>
     </>
   );
