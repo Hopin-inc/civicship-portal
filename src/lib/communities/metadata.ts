fbcb682ad40bf0c0b0179e0591aa15853cac24cd
宮下裕希 <hirokixyzrr@gmail.com>
2025-07-29 13:28:19 +0900

    不要な修正を削除

--- src/lib/communities/metadata.ts
+++ src/lib/communities/metadata.ts
@@ -73,7 +73,7 @@ const COMMUNITY_BASE_CONFIG: Record<string, CommunityBaseConfig> = {
     logoPath: "/communities/neo88/logo.jpg",
     squareLogoPath: "/communities/neo88/logo-square.jpg",
     ogImagePath: "https://storage.googleapis.com/prod-civicship-storage-public/asset/neo88/ogp.jpg",
-    enableFeatures: ["opportunities", "places", "points", "articles", "tickets", "prefectures","quests"],
+    enableFeatures: ["opportunities", "places", "points", "articles", "tickets", "prefectures"],
     rootPath: "/activities",
     adminRootPath: "/admin/reservations",
   },
