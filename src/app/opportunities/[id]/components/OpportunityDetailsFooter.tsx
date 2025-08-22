111602868b796db600bc49837d99130517e783a7
宮下裕希 <hirokixyzrr@gmail.com>
2025-08-22 10:53:01 +0900

    Questsの場合料金を表示しないようにする

--- src/app/opportunities/[id]/components/OpportunityDetailsFooter.tsx
+++ src/app/opportunities/[id]/components/OpportunityDetailsFooter.tsx
@@ -67,15 +67,15 @@ export const OpportunityDetailsFooter: React.FC<OpportunityDetailsFooterProps> =
         <div>
           <div>
             <p className="text-body-sm text-muted-foreground">1人あたり</p>
-              {price !== null ? (
+              {price != null ? (
                 <p className="text-body-lg font-bold">
                   {`${price.toLocaleString()}円〜`}
                 </p>
-              ) : (
+              ) : point == null ? (
                 <p className="text-body-lg font-bold text-muted-foreground/50">
                   料金未定
                 </p>
-              )}
+              ) : null}
               {point != null && (
                 <p>
                   <span className="font-bold text-lg">{point.toLocaleString()}pt</span>
