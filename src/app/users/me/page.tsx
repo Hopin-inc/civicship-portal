416369eeed2749b7710ac0ee8cfabe573c88ff8e
宮下裕希 <hirokixyzrr@gmail.com>
2025-09-01 09:39:55 +0900

    テスト用にローディング表示をLoadingIndicatorからテキスト表示に変更しました。

--- src/app/users/me/page.tsx
+++ src/app/users/me/page.tsx
@@ -31,7 +31,7 @@ export default function MyProfilePage() {
 
   // 認証中 or リダイレクト待ち → ローディング表示
   if (isAuthenticating || !currentUser) {
-    return <LoadingIndicator />;
+    return <div>loading</div>;
   }
 
   // 認証完了してるけど currentUser が null → 何も描画しない（push 発火済み）
