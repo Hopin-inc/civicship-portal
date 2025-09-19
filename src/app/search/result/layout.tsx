f5d407786254b8b3c80d35d9bc578d8249c29bbe
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-09-19 16:22:54 +0900

    Remove AuthProvider and RouteGuard wrapping from search result layout.

--- src/app/search/result/layout.tsx
+++ src/app/search/result/layout.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { metadata } from "./metadata";
-import { AuthProvider } from "@/contexts/AuthProvider";
-import { RouteGuard } from "@/components/auth/RouteGuard";
 
 export { metadata };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <AuthProvider>
-      <RouteGuard>{children}</RouteGuard>
-    </AuthProvider>
-  );
+  return <>{children}</>;
 }
