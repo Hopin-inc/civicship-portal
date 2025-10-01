034f66d86ec2a7bd221d27ce76d25e2e2627c6d6
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-10-01 15:54:26 +0900

    Add server-side hook to fetch the current user via GraphQL

---
+++ src/hooks/auth/serverAuth.ts
@@ -0,0 +1,35 @@
+import { executeServerGraphQLQuery } from "@/lib/graphql/server";
+import { GqlCurrentUserQuery, GqlCurrentUserQueryVariables } from "@/types/graphql";
+
+export async function getCurrentUserServer() {
+  const res = await executeServerGraphQLQuery<GqlCurrentUserQuery, GqlCurrentUserQueryVariables>(
+    GET_CURRENT_USER_SERVER_QUERY,
+    {},
+  );
+  return res.currentUser ?? null;
+}
+
+const GET_CURRENT_USER_SERVER_QUERY = `
+  query currentUser {
+    currentUser {
+      user {
+        id
+        name
+        memberships {
+          status
+          role
+          user {
+            id
+            name
+          }
+          community {
+            id
+            name
+          }
+          role
+          status
+        }
+      }
+    }
+  }
+`;
