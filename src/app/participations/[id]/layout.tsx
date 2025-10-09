36b4dac5b82254f782e2cc75440fcbb6052929ee
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-10-09 20:27:01 +0900

    Comment out metadata generation and fetching logic in multiple layouts.

--- src/app/participations/[id]/layout.tsx
+++ src/app/participations/[id]/layout.tsx
@@ -1,60 +1,61 @@
-import React from "react";
-import { Metadata } from "next";
-import { fallbackMetadata, DEFAULT_OPEN_GRAPH_IMAGE } from "@/lib/communities/metadata";
-import {
-  GetParticipationDocument,
-  GqlGetParticipationQuery,
-  GqlGetParticipationQueryVariables,
-  GqlParticipation,
-} from "@/types/graphql";
-import { apolloClient } from "@/lib/apollo";
-
-type Props = {
-  params: { id: string };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const id = params.id;
-  const res = await fetchParticipation(id);
-
-  if (!res) return fallbackMetadata;
-
-  const title = res.reservation?.opportunitySlot?.opportunity?.title;
-  const description = res.reservation?.opportunitySlot?.opportunity?.description;
-  const user = res.user?.name;
-
-  return {
-    title: `${title} | ${user}さんの関わり`,
-    description,
-    openGraph: {
-      type: "article",
-      title: `${title} | ${user}さんの関わり`,
-      description,
-      images: res.images?.[0]
-        ? [
-            {
-              url: res.images[0],
-              width: 1200,
-              height: 630,
-              alt: `${title} | ${user}さんの関わり`,
-            },
-          ]
-        : DEFAULT_OPEN_GRAPH_IMAGE,
-    },
-  };
-}
-
-async function fetchParticipation(id: string): Promise<GqlParticipation | null> {
-  const { data } = await apolloClient.query<
-    GqlGetParticipationQuery,
-    GqlGetParticipationQueryVariables
-  >({
-    query: GetParticipationDocument,
-    variables: { id },
-  });
-
-  return data.participation ?? null;
-}
+// import React from "react";
+// import { Metadata } from "next";
+// import { DEFAULT_OPEN_GRAPH_IMAGE, fallbackMetadata } from "@/lib/communities/metadata";
+// import {
+//   GetParticipationDocument,
+//   GqlGetParticipationQuery,
+//   GqlGetParticipationQueryVariables,
+//   GqlParticipation,
+// } from "@/types/graphql";
+// import { apolloClient } from "@/lib/apollo";
+//
+// type Props = {
+//   params: { id: string };
+// };
+//
+// export async function generateMetadata(input: Promise<Props>): Promise<Metadata> {
+//   const { params } = await input;
+//   const id = params.id;
+//   const res = await fetchParticipation(id);
+//
+//   if (!res) return fallbackMetadata;
+//
+//   const title = res.reservation?.opportunitySlot?.opportunity?.title;
+//   const description = res.reservation?.opportunitySlot?.opportunity?.description;
+//   const user = res.user?.name;
+//
+//   return {
+//     title: `${title} | ${user}さんの関わり`,
+//     description,
+//     openGraph: {
+//       type: "article",
+//       title: `${title} | ${user}さんの関わり`,
+//       description,
+//       images: res.images?.[0]
+//         ? [
+//             {
+//               url: res.images[0],
+//               width: 1200,
+//               height: 630,
+//               alt: `${title} | ${user}さんの関わり`,
+//             },
+//           ]
+//         : DEFAULT_OPEN_GRAPH_IMAGE,
+//     },
+//   };
+// }
+//
+// async function fetchParticipation(id: string): Promise<GqlParticipation | null> {
+//   const { data } = await apolloClient.query<
+//     GqlGetParticipationQuery,
+//     GqlGetParticipationQueryVariables
+//   >({
+//     query: GetParticipationDocument,
+//     variables: { id },
+//   });
+//
+//   return data.participation ?? null;
+// }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
--- src/app/users/[id]/layout.tsx
+++ src/app/users/[id]/layout.tsx
@@ -1,57 +1,48 @@
-import { Metadata } from "next";
-import {
-  GetUserFlexibleDocument,
-  GqlGetUserFlexibleQuery,
-  GqlGetUserFlexibleQueryVariables,
-  GqlUser,
-} from "@/types/graphql";
-import { apolloClient } from "@/lib/apollo";
-import { fallbackMetadata, DEFAULT_OPEN_GRAPH_IMAGE } from "@/lib/communities/metadata";
 import React from "react";
 
-type Props = {
-  params: { id: string };
-};
-
-export async function generateMetadata(input: Promise<Props>): Promise<Metadata> {
-  const { params } = await input;
-  const id = params.id;
-  const res = await fetchUser(id);
-
-  if (!res) return fallbackMetadata;
-
-  return {
-    title: res.name,
-    description: res.bio ?? "",
-    openGraph: {
-      type: "profile",
-      title: res.name,
-      description: res.bio ?? "",
-      images: res.image
-        ? [
-            {
-              url: res.image,
-              width: 1200,
-              height: 630,
-              alt: res.name,
-            },
-          ]
-        : DEFAULT_OPEN_GRAPH_IMAGE,
-    },
-  };
-}
-
-async function fetchUser(id: string): Promise<GqlUser | null> {
-  const { data } = await apolloClient.query<
-    GqlGetUserFlexibleQuery,
-    GqlGetUserFlexibleQueryVariables
-  >({
-    query: GetUserFlexibleDocument,
-    variables: { id },
-  });
-
-  return data.user ?? null;
-}
+// type Props = {
+//   params: { id: string };
+// };
+//
+// export async function generateMetadata(input: Promise<Props>): Promise<Metadata> {
+//   const { params } = await input;
+//   const id = params.id;
+//   const res = await fetchUser(id);
+//
+//   if (!res) return fallbackMetadata;
+//
+//   return {
+//     title: res.name,
+//     description: res.bio ?? "",
+//     openGraph: {
+//       type: "profile",
+//       title: res.name,
+//       description: res.bio ?? "",
+//       images: res.image
+//         ? [
+//             {
+//               url: res.image,
+//               width: 1200,
+//               height: 630,
+//               alt: res.name,
+//             },
+//           ]
+//         : DEFAULT_OPEN_GRAPH_IMAGE,
+//     },
+//   };
+// }
+//
+// async function fetchUser(id: string): Promise<GqlUser | null> {
+//   const { data } = await apolloClient.query<
+//     GqlGetUserFlexibleQuery,
+//     GqlGetUserFlexibleQueryVariables
+//   >({
+//     query: GetUserFlexibleDocument,
+//     variables: { id },
+//   });
+//
+//   return data.user ?? null;
+// }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
