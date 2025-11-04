1facde81a97e8aa85c6b858592522d7a56c1afcd
sigma-xing2 <78943811+sigma-xing2@users.noreply.github.com>
2025-11-04 09:32:42 +0900

    Revert "fix: exclude `_next/image` from middleware matcher for improved pattern handling"
    This reverts commit c725baca12f27659d17da3f442a9a04d41897ea3.

--- src/middleware.ts
+++ src/middleware.ts
@@ -53,7 +53,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     "/_next/image",
-    "/((?!_next/static|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 };
 
