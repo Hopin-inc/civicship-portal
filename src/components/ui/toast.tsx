8131323f411826a1da254ac400c441acccdc4793
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-11-07 14:46:00 +0900

    "Fix toast component formatting and update default class spacing."

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,25 +1,25 @@
-"use client"
+"use client";
 
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { useTheme } from "next-themes"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useTheme } from "next-themes";
 
 export function Toaster() {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <ToastContainer
       position="bottom-center"
       autoClose={3000}
       hideProgressBar={false}
-      newestOnTop
+      newestOnTop={true}
       closeOnClick
       rtl={false}
       pauseOnFocusLoss
       draggable
       pauseOnHover
       theme={theme === "dark" ? "dark" : "light"}
-      className="mx-8"
+      className="mx-10"
     />
-  )
+  );
 }
