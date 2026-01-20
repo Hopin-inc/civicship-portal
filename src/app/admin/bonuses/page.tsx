ba4eaf9521dac3425ed21bb9be1454652441d51f
naoki_sakata <709sakata@gmail.com>
2026-01-20 18:00:48 +0900

    Remove unnecessary console.log from bonuses admin page.

--- src/app/admin/bonuses/page.tsx
+++ src/app/admin/bonuses/page.tsx
@@ -51,7 +51,6 @@ export default function BonusesPage() {
   const { data, loading, error } = useQuery<GetSignupBonusConfigData>(GET_SIGNUP_BONUS_CONFIG, {
     variables: { communityId: COMMUNITY_ID },
   });
-  console.log(data);
 
   const { data: failedData } = useQuery<GetFailedSignupBonusesData>(GET_FAILED_SIGNUP_BONUSES, {
     variables: { communityId: COMMUNITY_ID },
